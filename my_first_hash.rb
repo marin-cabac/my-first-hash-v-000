2c52bda618a3108f8fb91572da87a2bc42775265
Marin Cabac <marin.cabac@gmail.com>
2018-02-13 13:55:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,7 +38,14 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
  #shipping_manifest.merge!("muskets" => 2, "gun powder" => 4)
-shipping_manifest["muskets"]=2
+ shipping_manifest = {
+                 "whale bone corsets" => 5,
+                 "porcelain vases" => 2,
+                 "oil paintings" => 3,
+                 "muskets" => 2, 
+                 "gun powder" => 4
+
+                }
 
 
   # add 4 gun powder to the shipping_manifest hash below
