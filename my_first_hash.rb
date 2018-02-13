01f8e621b176282bee17ac391b24d4bc72d72c22
Marin Cabac <marin.cabac@gmail.com>
2018-02-13 12:49:58 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-h=Hash.new
-h={"whale bone corsets"=>	5, "porcelain vases"	=>2, "oil paintings"	=>3}
+my_hash=Hash.new
+my_hash={"whale bone corsets"=>	5, "porcelain vases"	=>2, "oil paintings"	=>3}
 
 end
 
