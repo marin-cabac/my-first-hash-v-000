3e89af317da745b5fc88bb697bccf6e1ac28f486
Marin Cabac <marin.cabac@gmail.com>
2018-02-13 12:51:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 my_hash=Hash.new
-my_hash={"whale bone corsets"=>	5, "porcelain vases"	=>2, "oil paintings"	=>3}
+shipping_manifest={"whale bone corsets"=>	5, "porcelain vases"	=>2, "oil paintings"	=>3}
 
 end
 
